7c1b45e0a2c6b3c94dd9d2b55574d946eaaa0c87
nubilepenguin <peng523888394@gmail.com>
2020-01-03 12:43:52 +0800

    naive trading strategies

---
+++ classical_trading_strategies/double_moving_average.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+mpl.rcParams['font.family'] = 'serif'
+def load_financial_data(start_date, end_date, output_file):
+    try:
+        df = pd.read_pickle(output_file)
+        print('File data found...reading GOOG data')
+    except FileNotFoundError:
+        print('File not found...downloading the GOOG data')
+        df = data.DataReader('GOOG', 'yahoo', start_date, end_date)
+        df.to_pickle(output_file)
+    return df
+
+goog_data = load_financial_data(start_date = '2001-01-01',
+                                end_date = '2018-01-01',
+                                output_file = 'goog_data_large.pkl')
+
+def double_moving_average(financial_data, short_window, long_window):
+    signals = pd.DataFrame(index=financial_data.index)
+    signals['signal'] = 0.0
+    signals['short_mavg'] = financial_data['Close'].rolling(window=short_window,
+                                                            min_periods=1, center=False).mean()
+    signals['long_mavg'] = financial_data['Close'].rolling(window=long_window,
+                                                            min_periods=1, center=False).mean()
+    signals['signal'][short_window:] = np.where(
+        signals['short_mavg'][short_window:] > signals['long_mavg'][short_window:], 1.0, 0.0)
+    signals['orders'] = signals['signal'].diff()
+    return signals
+
+ts = double_moving_average(goog_data, 20, 100)
+fig = plt.figure()
+ax1 = fig.add_subplot(111, ylabel='Google price in $')
+goog_data["Adj Close"].plot(ax=ax1, color='g', lw=.5)
+ts["short_mavg"].plot(ax=ax1, color='r', lw=2.)
+ts["long_mavg"].plot(ax=ax1, color='b', lw=2.)
+
+ax1.plot(ts.loc[ts.orders == 1.0].index, goog_data["Adj Close"][ts.orders == 1.0], '^', markersize=7, color='k')
+ax1.plot(ts.loc[ts.orders == -1.0].index, goog_data["Adj Close"][ts.orders == -1.0], 'v', markersize=7, color='k')
+plt.legend(["Price","Short mavg","Long mavg","Buy","Sell"])
+plt.title("Double Moving Average Trading Strategy")
+plt.show()
+
+
---
+++ classical_trading_strategies/pairs_correlation.py
@@ -0,0 +1,82 @@
+import pandas as pd
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from statsmodels.tsa.stattools import coint
+
+mpl.rcParams['font.family'] = 'serif'
+# Set a seed value to make the experience reproducible
+np.random.seed(123)
+
+# Generate Symbol1 daily returns
+Symbol1_returns = np.random.normal(0, 1, 100)
+# Generate a series for symbol1 prices
+Symbol1_prices = pd.Series(np.cumsum(Symbol1_returns), name='Symbol1') + 10
+Symbol1_prices.plot(figsize=(15, 7))
+plt.show()
+
+# Generate a series for Symbol2 prices
+# We are going to mimikate the Symbol1 behavior
+noise = np.random.normal(0, 1, 100)
+Symbol2_prices = Symbol1_prices + 10 + noise
+Symbol2_prices.name = 'Symbol2'
+plt.title("Symbol 1 and Symbol 2 prices")
+Symbol1_prices.plot()
+Symbol2_prices.plot()
+plt.legend()
+plt.show()
+
+
+def zscore(series):
+    return (series - series.mean()) / np.std(series)
+
+score, pvalue, _ = coint(Symbol1_prices, Symbol2_prices)
+print(pvalue)
+ratios = Symbol1_prices / Symbol2_prices
+plt.title("Ration between Symbol 1 and Symbol 2 price")
+
+ratios.plot()
+plt.show()
+
+zscore(ratios).plot()
+plt.title("Z-score evolution")
+plt.axhline(zscore(ratios).mean(),color="black")
+plt.axhline(1.0, color="red")
+plt.axhline(-1.0, color="green")
+plt.show()
+
+ratios.plot()
+buy = ratios.copy()
+sell = ratios.copy()
+buy[zscore(ratios) > -1] = 0
+sell[zscore(ratios) < 1] = 0
+buy.plot(color="g", linestyle="None", marker="^")
+sell.plot(color="r", linestyle="None", marker="v")
+x1,x2,y1,y2 = plt.axis()
+plt.axis((x1,x2,ratios.min(),ratios.max()))
+plt.legend(["Ratio", "Buy Signal", "Sell Signal"])
+plt.show()
+
+symbol1_buy=Symbol1_prices.copy()
+symbol1_sell=Symbol1_prices.copy()
+symbol2_buy=Symbol2_prices.copy()
+symbol2_sell=Symbol2_prices.copy()
+
+Symbol1_prices.plot()
+symbol1_buy[zscore(ratios)>-1] = 0
+symbol1_sell[zscore(ratios)<1] = 0
+symbol1_buy.plot(color="g", linestyle="None", marker="^")
+symbol1_sell.plot(color="r", linestyle="None", marker="v")
+
+Symbol2_prices.plot()
+symbol2_buy[zscore(ratios)<1] = 0
+symbol2_sell[zscore(ratios)>-1] = 0
+symbol2_buy.plot(color="g", linestyle="None", marker="^")
+symbol2_sell.plot(color="r", linestyle="None", marker="v")
+
+
+x1,x2,y1,y2 = plt.axis()
+plt.axis((x1,x2,Symbol1_prices.min(),Symbol2_prices.max()))
+plt.legend(["Symbol1", "Buy Signal", "Sell Signal","Symbol2"])
+plt.show()
